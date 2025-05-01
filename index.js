c96f4bfb2cf269788f12b571995d08787fb79191
vishaldethe15 <dethevishal9@gmail.com>
2025-05-01 19:13:12 +0530

    Removed comments

--- index.js
+++ index.js
@@ -7,25 +7,17 @@ dotenv.config();
 
 const app = express();
 
-// === Middleware ===
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// === Basic Test Route ===
 app.get("/", (req, res) => {
-  res.send("Task Management API is running 🚀");
+  res.send("Task Management API is running ");
 });
-
-// === Add your routes here ===
-// import taskRoutes from './routes/task.routes.js';
-// app.use('/api/tasks', taskRoutes);
 
 app.use("/api/auth", authRouter);
 
-// === Start Server ===
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
