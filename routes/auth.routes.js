100847f39aed794ed1e579826968fdf376b680e1
vishaldethe15 <dethevishal9@gmail.com>
2025-05-01 17:12:35 +0530

    Utilized auth middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controllers.js";
+import { verifyRole, verifyUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(verifyUser, verifyRole("admin"), register);
 router.route("/login").post(login);
 
 export default router;
