7af060385c965dd0fa849002f8cfee43b10326bc
vishaldethe15 <dethevishal9@gmail.com>
2025-05-03 14:58:32 +0530

    Added new db connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes.js";
 import taskRouter from "./routes/task.routes.js";
+import { connectDB } from "./db/connect.js";
 
 dotenv.config();
 
@@ -21,6 +22,15 @@ app.use("/api/tasks", taskRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
