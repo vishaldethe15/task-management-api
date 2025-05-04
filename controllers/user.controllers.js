64d02f8e06c139460590e94c4ad730fca80f00e1
vishaldethe15 <dethevishal9@gmail.com>
2025-05-04 14:02:50 +0530

    Added update and delete user controller

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/User.model.js";
+import bcrypt from "bcryptjs";
 
 export const getAllUsers = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
@@ -25,5 +26,77 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-export const updateUserById = async (req, res) => {};
-export const deleteUserById = async (req, res) => {};
+export const updateUserById = async (req, res) => {
+  const { id } = req.params;
+  const { userId, role: userRole } = req.user;
+  const { name, email, password, role } = req.body;
+
+  try {
+    if (userRole === "user" && userId !== id) {
+      return res.status(403).json({
+        message: "Users can only update their own profile",
+      });
+    }
+
+    const updateFields = {};
+
+    if (name) updateFields.name = name;
+    if (email) updateFields.email = email;
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      updateFields.password = hashedPassword;
+    }
+    if (role) {
+      if (userRole !== "admin") {
+        return res.status(403).json({
+          message: "Only admin can update user roles",
+        });
+      }
+      updateFields.role = role;
+    }
+
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      id,
+      { $set: updateFields },
+      { new: true, runValidators: true }
+    ).select("_id name email role");
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const deleteUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await UserModel.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "User deleted successfully",
+      user: {
+        id: deletedUser._id,
+        name: deletedUser.name,
+        email: deletedUser.email,
+        role: deletedUser.role,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
